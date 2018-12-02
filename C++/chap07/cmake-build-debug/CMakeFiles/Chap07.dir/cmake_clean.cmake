95ba9a481b758271c532f4dde2f292085d255f53
hyunkwanko <cjdshrwkd11@gmail.com>
2018-12-02 16:29:10 +0900

    C++

---
+++ C++/chap07/cmake-build-debug/CMakeFiles/Chap07.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chap07.dir/main.cpp.o"
+  "Chap07.pdb"
+  "Chap07"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chap07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/chap07/main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+
+/* 예제 1 */
+//class CPoint {
+//private:
+//    int x,y;
+//public:
+//    CPoint(int a = 0, int b = 0) : x(a), y(b) {}
+//    CPoint Sum(const CPoint &Po) { return CPoint(x + Po.x, y + Po.y); }
+//    void Print() { cout << "(" << x << ", " << y << ")" << endl; }
+//};
+//
+//int main(void){
+//    CPoint P1(1, 1);
+//    CPoint P2(2, 2);
+//    CPoint P3 = P1.Sum(P2); // Sum 함수 호출
+//
+//    P3.Print();
+//
+//    return 0;
+//}
+
+/* 예제 2 */
+//class CPoint {
+//private:
+//    int x,y;
+//public:
+//    CPoint(int a = 0, int b = 0) : x(a), y(b) {}
+//    CPoint operator+(const CPoint &Po) { return CPoint(x + Po.x, y + Po.y); }
+//    void Print() { cout << "(" << x << ", " << y << ")" << endl; }
+//};
+//
+//int main(void){
+//    CPoint P1(1, 1);
+//    CPoint P2(2, 2);
+//    CPoint P3 = P1 + P2; // Sum 함수 호출
+//
+//    P3.Print();
+//
+//    return 0;
+//}
+
+/* 예제 3 */
+//class CPoint {
+//private:
+//    int x,y;
+//public:
+//    CPoint(int a = 0, int b = 0) : x(a), y(b) {}
+//    void Print() { cout << "(" << x << ", " << y << ")" << endl; }
+//
+//    friend CPoint operator+(const CPoint &Po1, const CPoint &Po2);
+//};
+//
+//CPoint operator+(const CPoint &Po1, const CPoint &Po2){
+//    return CPoint(Po1.x + Po2.x, Po1.y + Po2.y);
+//}
+//
+//int main(void){
+//    CPoint P1(1, 1);
+//    CPoint P2(2, 2);
+//    CPoint P3 = P1 + P2; // Sum 함수 호출
+//
+//    P3.Print();
+//
+//    return 0;
+//}
+
+/* 예제 4 */
+//class CPoint {
+//private :
+//    int x, y;
+//public :
+//    CPoint(int a = 0,int b = 0):x(a),y(b){}
+//    CPoint operator+(const CPoint &Po) { return CPoint(x + Po.x, y + Po.y); }
+//    CPoint operator+(int a) { return CPoint(x + a, y + a); }
+//    CPoint operator-(const CPoint &Po) { return CPoint(x - Po.x, y - Po.y); }
+//    CPoint operator-(int a) { return CPoint(x - a, y - a); }
+//    CPoint operator*(int a) { return CPoint(x * a, y * a); }
+//    CPoint operator/(int a) { return CPoint(x / a, y / a); }
+//    void Print(CPoint Po = CPoint(0, 0)){ cout << "(" << x << "," << y << ")" << endl; }
+//};
+//
+//int main(void){
+//    CPoint P1(2, 2), P2(4, 4);
+//    int a = 2;
+//    (P1 + P2).Print();
+//    (P1 + a).Print();
+//    (P1 - P2).Print();
+//    (P1 - a).Print();
+//    (P1 * a).Print();
+//    (P1 / a).Print();
+//    return 0;
+//}
+
+/* 예제 5 */
+//class CPoint {
+//private :
+//    int x, y;
+//public :
+//    CPoint(int a = 0, int b = 0) : x(a), y(b) {}
+//    CPoint operator-() { return CPoint(-x, -y); } // - 부호연산자
+////    CPoint operator-() { x = -x; y = -y; return CPoint(x, y); } // 응용 : 음수 부호(-) 연산자의 의미 변경
+//    void Print(){ cout << "(" << x << "," << y << ")" << endl; }
+//};
+//
+//int main(void) {
+//    CPoint P1(2, 2);
+//    CPoint P2 = -P1;
+//    CPoint P3 = -(-P1);
+//
+//    P1.Print();
+//    P2.Print();
+//    P3.Print();
+//    return 0;
+//}
+
+/* 예제 6 */
+//class CPoint { // 위의 예제 해결 방안 -> 참조 반환
+//private :
+//    int x; int y;
+//public :
+//    CPoint(int a = 0, int b = 0): x(a), y(b) {}
+//    CPoint &operator-() { x = -x; y = -y; return (*this); }
+//    void Print(){ cout << "(" << x << "," << y << ")" << endl; }
+//};
+//
+//int main(void) {
+//    CPoint P1(2, 2);
+//    CPoint P2 = -(-P1); // P1(2,2) 두 번 변함, P2(2,2)
+//    P1.Print();
+//    P2.Print();
+//    return 0;
+//}
+
+/* 예제 7 */
+//class CPoint {
+//private :
+//    int x, y;
+//public :
+//    CPoint(int a = 0, int b = 0): x(a), y(b) {}
+//    CPoint &operator++() { x++; y++; return (*this); }// 전위 증가 연산자
+//    void Print(){ cout << "(" << x << "," << y << ")" << endl; }
+//};
+//int main(void) {
+//    CPoint P1(1, 1);
+//    CPoint P2 = ++P1; // P1 (2, 2), P2 (2, 2)
+//    CPoint P3 = ++(++P1); // P1 (4, 4), P3 (4, 4)
+//    P1.Print();
+//    P2.Print();
+//    P3.Print();
+//    return 0;
+//}
