f40231402db1a1442734d888cbb0b21f4f39e8db
hyunkwanko <cjdshrwkd11@gmail.com>
2018-12-07 23:05:29 +0900

    C++

---
+++ C++/chap08/cmake-build-debug/CMakeFiles/chap08.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chap08.dir/main.cpp.o"
+  "chap08.pdb"
+  "chap08"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chap08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/chap08/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+
+/* 예제 1 */
+//class CCricle {
+//public:
+//    int x, y;
+//    double Radius;
+//public:
+//    double GetArea() {
+//        return (PI * Radius * Radius);
+//    }
+//};
+//
+//class CSphere : public CCricle { // CCircle로부터 상속
+//public:
+//    int z;
+//public:
+//    double GetVolume() {
+//        return ((4.0/3.0) * PI * Radius * Radius * Radius);
+//    }
+//};
+//
+//int main(){
+//    CSphere Sph; // Sph: x, y, Radius 상속
+//    Sph.x = 1;
+//    Sph.y = 1;
+//    Sph.z = 1;
+//    Sph.Radius = 5;
+//
+//    cout << "구의 표면적 : " << Sph.GetArea() << endl; // Sph:GetArea 상속
+//    cout << "구의 부피 : " << Sph.GetVolume() << endl;
+//    return 0;
+//}
+
+/* 예제 2 */
+//class CCircle {
+//protected :
+//    int x, y; double Radius;
+//public :
+//    CCircle(int a, int b, double r) : x(a), y(b), Radius(r) { }
+//    double GetArea() { return (PI * Radius * Radius); } };
+//class CSphere : public CCircle {
+//private :
+//    int z;
+//public :
+//    CSphere(int a, int b, int c, double r) : CCircle(a, b, r), z(c) { }
+//    double GetVolume() { return ((4.0/3.0) * PI * Radius * Radius * Radius); }
+//};
+//
+//int main(void) {
+//    CSphere Sph(1, 2, 3, 4);
+//    cout << Sph.GetVolume() << endl;
+//    return 0;
+//}
+
+/* 예제 3 */
+class CCircle {
+protected :
+    int x, y; double Radius;
+public :
+    CCircle(int a, int b, double r) : x(a), y(b), Radius(r) { }
+    double GetArea() { return (PI * Radius * Radius); }
+};
+
+class CSphere : public CCircle {// derived가 class이므로 디폴트로 private 상속
+private :
+    int z;
+public :
+    CSphere(int a, int b, int c, double r) : CCircle(a, b, r), z(c) { }
+    double GetVolume() { return ((4.0/3.0) * PI * Radius * Radius * Radius); }
+};
+
+int main(void) {
+    CSphere Sph(1, 1, 1, 1);
+    cout << Sph.GetArea() << endl;
+    cout << Sph.GetVolume() << endl;
+    return 0;
+}
